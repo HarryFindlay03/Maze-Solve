315a4cc0d300281bcf7e78bdf981607d1c77ee90
Harry Findlay <harryfindlay@outlook.com>
2023-03-06 20:51:27 +0000

    basic working Node clasS

--- a_star_solver.py
+++ a_star_solver.py
@@ -2,9 +2,19 @@ from queue import PriorityQueue
 from typing import List
 
 class Node:
-    def __init__(self, row, col, parent=None) -> None:
+    def __init__(self, pos: tuple, parent=None) -> None:
         self.parent = parent
-        self.pos = (row, col)
+        self.pos = pos
+
+    def __hash__(self) -> int:
+        return hash(self.pos)
+
+    def __eq__(self, other) -> bool:
+        return self.pos == other.pos
+    
+    def __lt__(self, other) -> bool:
+        return self.pos < other.pos
+    
 
 def manhattan_distance(start: tuple, goal: tuple) -> int:
     """
@@ -27,37 +37,46 @@ def get_neighbours(arr: List[List[str]], node: tuple, visited: set()) -> List[tu
     return neighbours
 
 
-def reconstruct_path(arr: List[List[str]], goal: tuple, visited: List[Node]):
+def reconstruct_path(start: Node, goal: Node, visited: set[Node]):
     """
-    Take a list of nodes and go backwards adding all the parents to a visited list,
+        Take a set of nodes and go backwards adding all the parents to a visited list,
     until start is reached, then return the reverse of this list.
+
+    Args:
+        start (Node): The start node to reconstruct the path from, this will be the finish gate
+        goal (Node): The final node, one with no parent, this will be the start gate
+        visited (set[Node]): The set of visited nodes.
     """
-    pass
+    path = []
+    path.append(start.pos)
+
+
+    
 
 
 def a_star(arr: List[List[str]], start: tuple, goal: tuple):
     visited = set()
 
     yet_to_visit = PriorityQueue()
-    yet_to_visit.put((manhattan_distance(start, goal), start))
+    yet_to_visit.put((manhattan_distance(start, goal), Node(start)))
 
     while not yet_to_visit.empty():
-        # get valid neighbours of the best item in the priority queue
+        # get valid neighbours of best item in the priority queue
         curr = yet_to_visit.get()
-        visited.add(curr[1])
+        visited.add(curr[1].pos)
 
-        if curr[1] == goal:
+        if curr[1].pos == goal:
             return visited
         
-        neighbours = get_neighbours(arr, curr[1], visited)
+        neighbours = get_neighbours(arr, curr[1].pos, visited)
 
-        # if there are no valid neighbours get the next value from yet_to_visit
+        # if there are no valid neighbours, then get the next value from yet_to_visit
         if len(neighbours) == 0:
             continue
-         
+
         for neighbour in neighbours:
             if neighbour not in visited:
-                yet_to_visit.put((manhattan_distance(neighbour, goal), neighbour))
+                yet_to_visit.put((manhattan_distance(neighbour, goal), Node(neighbour, parent=curr[1])))
 
 
 
